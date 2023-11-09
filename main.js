914754fee8096f5eb43b19caed1e5d00846fb1d5
Thiago Amim <thiagoamim11@gmail.com>
2023-11-09 15:24:10 -0300

    resolvendo exercicios em javascript

---
+++ main.js
@@ -0,0 +1,95 @@
+
+// 1.Crie um script que exiba a mensagem "Hello World!" em um alerta no navegador
+
+alert("Hello World");
+
+//2.Crie um script que declare duas variáveis e exiba o resultado da soma entre elas.
+
+let numero1 = 2;
+var numero2 = 5;
+
+let resultado = numero1 + numero2;
+
+console.log(resultado);
+
+//3.Crie um script que declare uma variável e verifique se o seu valor é um número. Se for, exiba a mensagem "É um número", caso contrário, exiba a mensagem "Não é um número".
+
+let variavel = 10;
+
+if (typeof variavel == 'number') {
+    alert("É um numero");
+}
+
+else {
+    alert("Não é um numero");
+}
+
+//4.Crie um script que declare uma variável e verifique se o seu valor é uma string. Se for, exiba a mensagem "É uma string", caso contrário, exiba a mensagem "Não é uma string".
+
+let minhaVariavel = "Meu nome é Thiago";
+
+if (typeof minhaVariavel == 'string') {
+    console.log("É uma string");
+}
+
+else {
+    console.log("Não é uma string nem aqui nem na china");
+}
+
+//5.Crie um script que declare uma variável e verifique se o seu valor é um booleano. Se for, exiba a mensagem "É um booleano", caso contrário, exiba a mensagem "Não é um booleano".
+
+
+let idade = 34;
+
+if (idade == 34) {
+    console.log("É booleano");
+}
+
+else {
+    console.log("Não é booleano");
+}
+
+//6.Crie um script que declare duas variáveis e exiba o resultado da subtração entre elas. 
+let anoAtual = 2023;
+let anoNascimento = 1988;
+
+console.log(MinhaIdade = anoAtual - anoNascimento);
+
+//7.Crie um script que declare duas variáveis e exiba o resultado da multiplicação entre elas.
+
+let variavel1 = 2;
+let variavel2 = 210;
+let multiplicacao = 0
+
+console.log(multiplicacao = variavel1 * variavel2);
+
+//8.Crie um script que declare duas variáveis e exiba o resultado da divisão entre elas.
+
+let laranja = 100;
+let faca = 2;
+let corte = laranja / faca;
+
+console.log(corte);
+
+//9.Crie um script que declare uma variável e verifique se o seu valor é um número par. Se for, exiba a mensagem "É um número par", caso contrário, exiba a mensagem "Não é um número par".
+let exercicio9 = 9;
+
+if (typeof exercicio9 === 'number' && exercicio9 % 2 === 0) {
+    console.log("Esse numero é par");
+}
+
+else {
+    console.log("Esse numero é impar");
+}
+
+
+//10. Crie um script que declare uma variável e verifique se o seu valor é um número ímpar. Se for, exiba a mensagem "É um número ímpar", caso contrário, exiba a mensagem "Não é um número ímpar".
+let exercicio10 = 10;
+
+if (typeof exercicio10 === 'number' && exercicio10 % 2 === 0) {
+    console.log("Esse numero é par");
+}
+
+else {
+    console.log("Esse numero é impar");
+}
